5ac7c9496cd2af7ec608484eaa5bd5bb29da6fa5
Damian Pereira <damianpereira86@gmail.com>
2023-12-03 12:37:24 -0300

    Add support for C#, Java, Python and Selenium

--- popup/popup.js
+++ popup/popup.js
@@ -1,53 +1,97 @@
 function enableButton(button) {
     button.classList.add('enabled');
     button.classList.remove('disabled');
-};
+}
 
 function disableButton(button) {
     button.classList.add('disabled');
     button.classList.remove('enabled');
-};
+}
+
+function disableLanguageOptionsForCypress() {
+    javaBtn.classList.remove('available');
+    csharpBtn.classList.remove('available');
+    pythonBtn.classList.remove('available');
+}
+
+function enableLanguageOptions() {
+    javaBtn.classList.add('available');
+    csharpBtn.classList.add('available');
+    pythonBtn.classList.add('available');
+}
 
 function pickOption(option) {
-    let enable, disable;
-
     switch (option) {
         case 'javascript':
-            enable = javascriptBtn;
-            disable = typescriptBtn;
+            enableButton(javascriptBtn);
+            disableButton(typescriptBtn);
+            disableButton(javaBtn);
+            disableButton(csharpBtn);
+            disableButton(pythonBtn);
             break;
 
         case 'typescript':
-            enable = typescriptBtn;
-            disable = javascriptBtn;
+            enableButton(typescriptBtn);
+            disableButton(javascriptBtn);
+            disableButton(javaBtn);
+            disableButton(csharpBtn);
+            disableButton(pythonBtn);
+            break;
+
+        case 'java':
+            if (javaBtn.classList.contains('available')) {
+                enableButton(javaBtn);
+                disableButton(javascriptBtn);
+                disableButton(typescriptBtn);
+                disableButton(csharpBtn);
+                disableButton(pythonBtn);
+            }
+            break;
+
+        case 'csharp':
+            if (csharpBtn.classList.contains('available')) {
+                enableButton(csharpBtn);
+                disableButton(javascriptBtn);
+                disableButton(typescriptBtn);
+                disableButton(javaBtn);
+                disableButton(pythonBtn);
+            }
+            break;
+
+        case 'python':
+            if (pythonBtn.classList.contains('available')) {
+                enableButton(pythonBtn);
+                disableButton(javascriptBtn);
+                disableButton(typescriptBtn);
+                disableButton(javaBtn);
+                disableButton(csharpBtn);
+            }
             break;
 
         case 'cypress':
-            enable = cypressBtn;
-            disable = playwrightBtn;
+            enableButton(cypressBtn);
+            disableButton(playwrightBtn);
+            disableButton(seleniumBtn);
+            disableLanguageOptionsForCypress();
             break;
 
         case 'playwright':
-            enable = playwrightBtn;
-            disable = cypressBtn;
+            enableButton(playwrightBtn);
+            disableButton(cypressBtn);
+            disableButton(seleniumBtn);
+            enableLanguageOptions();
             break;
 
-        case 'pom-on':
-            enable = pomOnBtn;
-            disable = pomOffBtn;
-            break;
-
-        case 'pom-off':
-            enable = pomOffBtn;
-            disable = pomOnBtn;
+        case 'selenium':
+            enableButton(seleniumBtn);
+            disableButton(cypressBtn);
+            disableButton(playwrightBtn);
+            enableLanguageOptions();
             break;
 
         default:
             break;
     }
-
-    enableButton(enable);
-    disableButton(disable);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -56,63 +100,60 @@ document.addEventListener('DOMContentLoaded', () => {
         const options = {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
             },
-            body: JSON.stringify(payload)
+            body: JSON.stringify(payload),
         };
-        fetch(`${OPENAI_PROXY_BASE_URL}${ENDPOINTS.GENERATE_TEST_IDEAS}`, options).then(() => { });
+        fetch(`${OPENAI_PROXY_BASE_URL}${ENDPOINTS.GENERATE_TEST_IDEAS}`, options).then(() => {});
 
         const currentTabUrl = tabs[0].url;
 
-        chrome.storage.local.get([
-            STORAGE.ELEMENT_PICKED,
-            STORAGE.SITE_URL,
-            STORAGE.LANGUAGE_SELECTED,
-            STORAGE.FRAMEWORK_SELECTED,
-            STORAGE.POM
-        ], data => {
+        chrome.storage.local.get(
+            [
+                STORAGE.ELEMENT_PICKED,
+                STORAGE.SITE_URL,
+                STORAGE.LANGUAGE_SELECTED,
+                STORAGE.FRAMEWORK_SELECTED,
+                STORAGE.POM,
+            ],
+            (data) => {
+                if (!data[STORAGE.SITE_URL] || data[STORAGE.SITE_URL] !== currentTabUrl) {
+                    chrome.storage.local.remove([STORAGE.ELEMENT_PICKED, STORAGE.ELEMENT_SOURCE]);
+                    chrome.storage.local.set({ [STORAGE.SITE_URL]: currentTabUrl });
+                }
 
-            if (!data[STORAGE.SITE_URL] || data[STORAGE.SITE_URL] !== currentTabUrl) {
-                chrome.storage.local.remove([STORAGE.ELEMENT_PICKED, STORAGE.ELEMENT_SOURCE]);
-                chrome.storage.local.set({ [STORAGE.SITE_URL]: currentTabUrl });
-            }
+                if (data[STORAGE.ELEMENT_PICKED]) {
+                    automateBtn.disabled = false;
+                    generateTestIdeasBtn.disabled = false;
+                    checkAccessibilityBtn.disabled = false;
+                } else {
+                    automateBtn.disabled = true;
+                    generateTestIdeasBtn.disabled = true;
+                    checkAccessibilityBtn.disabled = true;
+                }
 
-            if (data[STORAGE.ELEMENT_PICKED]) {
-                automateBtn.disabled = false;
-                generateTestIdeasBtn.disabled = false;
-                checkAccessibilityBtn.disabled = false;
-            } else {
-                automateBtn.disabled = true;
-                generateTestIdeasBtn.disabled = true;
-                checkAccessibilityBtn.disabled = true;
-            }
+                if (data[STORAGE.FRAMEWORK_SELECTED]) {
+                    pickOption(data[STORAGE.FRAMEWORK_SELECTED]);
+                } else {
+                    pickOption(OPTION.PLAYWRIGHT);
+                    chrome.storage.local.set({ [STORAGE.FRAMEWORK_SELECTED]: OPTION.PLAYWRIGHT });
+                }
 
-            if (data[STORAGE.FRAMEWORK_SELECTED]) {
-                pickOption(data[STORAGE.FRAMEWORK_SELECTED]);
-            } else {
-                pickOption(OPTION.CYPRESS);
-                chrome.storage.local.set({ [STORAGE.FRAMEWORK_SELECTED]: OPTION.CYPRESS });
-            }
+                if (data[STORAGE.LANGUAGE_SELECTED]) {
+                    pickOption(data[STORAGE.LANGUAGE_SELECTED]);
+                } else {
+                    pickOption(OPTION.JAVASCRIPT);
+                    chrome.storage.local.set({ [STORAGE.LANGUAGE_SELECTED]: OPTION.JAVASCRIPT });
+                }
 
-            if (data[STORAGE.LANGUAGE_SELECTED]) {
-                pickOption(data[STORAGE.LANGUAGE_SELECTED]);
-            } else {
-                pickOption(OPTION.JAVASCRIPT);
-                chrome.storage.local.set({ [STORAGE.LANGUAGE_SELECTED]: OPTION.JAVASCRIPT });
-            }
-
-            if (data[STORAGE.POM]) {
-                pickOption('pom-on');
-            } else {
-                pickOption('pom-off');
-                chrome.storage.local.set({ [STORAGE.POM]: false });
-            }
-        });
+                chrome.storage.local.set({ [STORAGE.POM]: true });
+            },
+        );
     });
 });
 
 cypressBtn.addEventListener('click', () => {
-    pickOption(OPTION.CYPRESS)
+    pickOption(OPTION.CYPRESS);
     chrome.storage.local.set({ [STORAGE.FRAMEWORK_SELECTED]: OPTION.CYPRESS });
 });
 
@@ -121,36 +162,43 @@ playwrightBtn.addEventListener('click', () => {
     chrome.storage.local.set({ [STORAGE.FRAMEWORK_SELECTED]: OPTION.PLAYWRIGHT });
 });
 
+seleniumBtn.addEventListener('click', () => {
+    pickOption(OPTION.SELENIUM);
+    chrome.storage.local.set({ [STORAGE.FRAMEWORK_SELECTED]: OPTION.SELENIUM });
+});
+
 javascriptBtn.addEventListener('click', () => {
     pickOption(OPTION.JAVASCRIPT);
     chrome.storage.local.set({ [STORAGE.LANGUAGE_SELECTED]: OPTION.JAVASCRIPT });
 });
 
 typescriptBtn.addEventListener('click', () => {
-    pickOption(OPTION.TYPESCRIPT)
+    pickOption(OPTION.TYPESCRIPT);
     chrome.storage.local.set({ [STORAGE.LANGUAGE_SELECTED]: OPTION.TYPESCRIPT });
 });
 
-pomOnBtn.addEventListener('click', () => {
-    pickOption('pom-on')
-    chrome.storage.local.set({ [STORAGE.POM]: true });
+javaBtn.addEventListener('click', () => {
+    pickOption(OPTION.JAVA);
+    chrome.storage.local.set({ [STORAGE.LANGUAGE_SELECTED]: OPTION.JAVA });
 });
 
-pomOffBtn.addEventListener('click', () => {
-    pickOption('pom-off')
-    chrome.storage.local.set({ [STORAGE.POM]: false });
+csharpBtn.addEventListener('click', () => {
+    pickOption(OPTION.CSHARP);
+    chrome.storage.local.set({ [STORAGE.LANGUAGE_SELECTED]: OPTION.CSHARP });
+});
+
+pythonBtn.addEventListener('click', () => {
+    pickOption(OPTION.PYTHON);
+    chrome.storage.local.set({ [STORAGE.LANGUAGE_SELECTED]: OPTION.PYTHON });
 });
 
 // Update message and enable picker
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        if (request.source === "stream" && request.status == "finished") {
-            pickerBtn.disabled = false;
-            statusDescription.textContent = MESSAGES.SUCCESS;
-        }
-        else if (request.source === "stream" && request.status == "error") {
-            pickerBtn.disabled = false;
-            statusDescription.textContent = MESSAGES.FAILED;
-        }
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.source === 'stream' && request.status == 'finished') {
+        pickerBtn.disabled = false;
+        statusDescription.textContent = MESSAGES.SUCCESS;
+    } else if (request.source === 'stream' && request.status == 'error') {
+        pickerBtn.disabled = false;
+        statusDescription.textContent = MESSAGES.FAILED;
     }
-);
+});
--- scripts/constants.js
+++ scripts/constants.js
@@ -1,4 +1,4 @@
-const OPENAI_PROXY_BASE_URL = 'https://openai-api-proxy-rtu664353a-uc.a.run.app'
+const OPENAI_PROXY_BASE_URL = 'https://openai-api-proxy-rtu664353a-uc.a.run.app';
 
 const STORAGE = {
     SITE_URL: 'site-url',
@@ -9,14 +9,18 @@ const STORAGE = {
     AUTOMATED_TESTS: 'automated-tests',
     TEST_IDEAS: 'test-ideas',
     POM: 'pom',
-    IDEAS: 'ideas'
+    IDEAS: 'ideas',
 };
 
 const OPTION = {
     CYPRESS: 'cypress',
     PLAYWRIGHT: 'playwright',
+    SELENIUM: 'selenium',
     JAVASCRIPT: 'javascript',
     TYPESCRIPT: 'typescript',
+    JAVA: 'java',
+    CSHARP: 'csharp',
+    PYTHON: 'python',
 };
 
 const ACTION = {
@@ -24,7 +28,7 @@ const ACTION = {
     ELEMENT_SOURCE: 'element-source',
     START_PICKING: 'start-picking',
     HIGHLIGHT_ELEMENT: 'highlight-selected-element',
-    UNHIGHLIGHT_ELEMENT: 'unhighlight-selected-element'
+    UNHIGHLIGHT_ELEMENT: 'unhighlight-selected-element',
 };
 
 const MESSAGES = {
@@ -34,7 +38,7 @@ const MESSAGES = {
     SUCCESS: 'Success!',
     COPIED: 'Copied to Clipboard!',
     FAILED: 'Something happened, please try again :(',
-    TOO_LARGE: "The element selected is too large, please try again with a smaller element.",
+    TOO_LARGE: 'The element selected is too large, please try again with a smaller element.',
 };
 
 const RESULT = {
@@ -53,14 +57,18 @@ const FEATURE = {
     GENERATE_TEST_IDEAS: 'test-ideas',
     AUTOMATE_TESTS: 'automated-tests',
     AUTOMATE_IDEAS: 'automate-ideas',
-    CHECK_ACCESSIBILITY: 'check-accessibility'
+    CHECK_ACCESSIBILITY: 'check-accessibility',
 };
 
 const mainControls = document.getElementById('main-controls');
 const cypressBtn = document.getElementById('cypress');
 const playwrightBtn = document.getElementById('playwright');
+const seleniumBtn = document.getElementById('selenium');
 const javascriptBtn = document.getElementById('javascript');
 const typescriptBtn = document.getElementById('typescript');
+const javaBtn = document.getElementById('java');
+const csharpBtn = document.getElementById('csharp');
+const pythonBtn = document.getElementById('python');
 const pomOnBtn = document.getElementById('pom-on');
 const pomOffBtn = document.getElementById('pom-off');
 const pickerBtn = document.getElementById('start-picking');
